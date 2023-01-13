90378029c73d8b07e30cfb47e8856ca56b86628c
Roman-code-web <selenerg.275@gmail.com>
2023-01-13 09:23:11 -0500

    import , export y configuracion de rutas

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { DetalleComponent } from './detalle/detalle.component';
+import { ComponentesModule } from '../componentes/componentes.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    DetalleComponent
+    DetalleComponent,
+    NotFoundComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentesModule
   ]
 })
 export class PagesModule { }
